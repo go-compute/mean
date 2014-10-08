0644824e1a9e5bf5826b18f44f3e9ed0295113ab
kgryte <kgryte@gmail.com>
2014-10-07 18:38:58 -0700

    [UPDATE] formatted.

--- lib/mean.go
+++ lib/mean.go
@@ -2,15 +2,15 @@ package mean
 
 /**
 * Calculates the arithmetic mean of a numeric array.
-*/
-func Mean( arr []float64 ) ( mean float64 ) {
+ */
+func Mean(arr []float64) (mean float64) {
 	N := 0
 	delta := 0.0
 	mean = 0
 	for _, x := range arr {
 		N += 1
 		delta = x - mean
-		mean += delta / float64( N )
+		mean += delta / float64(N)
 	}
-	return mean;
+	return mean
 }
--- lib/mean_test.go
+++ lib/mean_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-func TestMean( t *testing.T ) {
-	var data []float64 = make( []float64, 101 )
-	for i := 0; i < len( data ); i++ {
-		data[ i ] = float64( i )
+func TestMean(t *testing.T) {
+	var data []float64 = make([]float64, 101)
+	for i := 0; i < len(data); i++ {
+		data[i] = float64(i)
 	}
-	mu := Mean( data )
+	mu := Mean(data)
 	if mu != 50 {
-		t.Errorf( "Mean() = %d; Expected = 50", mu )
+		t.Errorf("Mean() = %d; Expected = 50", mu)
 	}
 }
