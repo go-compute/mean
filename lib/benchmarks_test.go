77e66f510dec3ffe9798c239b53f2992a3e0a232
kgryte <kgryte@gmail.com>
2014-10-08 19:02:22 -0700

    [UPDATE] formatting.

--- lib/benchmarks_test.go
+++ lib/benchmarks_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-func BenchmarkMean( b *testing.B ) {
+func BenchmarkMean(b *testing.B) {
 	var data []float64 = make([]float64, 301)
 	for i := 0; i < len(data); i++ {
 		data[i] = float64(i)
 	}
 	b.ResetTimer()
 	for j := 0; j < b.N; j++ {
-		Mean( data )
+		Mean(data)
 	}
 }
--- lib/mean.go
+++ lib/mean.go
@@ -2,7 +2,7 @@
 package mean
 
 // Computes the arithmetic mean of a numeric array.
-func Mean( arr []float64) (mean float64) {
+func Mean(arr []float64) (mean float64) {
 	N := 0
 	delta := 0.0
 	mean = 0
